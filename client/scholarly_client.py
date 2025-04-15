12d735934a75b776eaac801554c620db67812500
galitz-matt <galitz.matthew@gmail.com>
2025-04-15 14:02:24 -0400

    Fetch publications for PI

---
+++ client/scholarly_client.py
@@ -0,0 +1,32 @@
+from scholarly import scholarly
+
+class ScholarlyClient:
+
+    @staticmethod
+    def get_author_profile(name, affiliation):
+        search_query = scholarly.search_author(name)
+        for result in search_query:
+            if affiliation and affiliation.lower() not in result['affiliation'].lower():
+                continue
+            return scholarly.fill(result)
+        return None
+
+    @staticmethod
+    def get_publications(profile):
+        if not profile:
+            return []
+        publications = []
+        for pub in profile["publications"][:10]:
+            filled = scholarly.fill(pub)
+
+            pub_url = filled.get("pub_url")
+            if not pub_url and "cluster_id" in filled:
+                pub_url = f"https://scholar.google.com/scholar?cluster={filled['cluster_id']}"
+
+            publications.append({
+                "title": filled["bib"]["title"],
+                "year": filled["bib"].get("pub_year", "N/A"),
+                "citations": filled.get("num_citations", 0),
+                "url": pub_url or "N/A"
+            })
+        return publications
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from client.http_client import HttpClient
 from scrapers.uva_scraper import UVAScraper
 import logging
+from client.scholarly_client import ScholarlyClient
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    scraper = UVAScraper(HttpClient())
-    print(scraper.scrape_faculty_profiles())
+logging.basicConfig(level=logging.INFO)
+
+# scraper = UVAScraper(HttpClient())
+# profiles = scraper.scrape_faculty_profiles()
+
+name = "Bryan Berger"
+affiliation = "University of Virginia"
+
+client = ScholarlyClient()
+author_profile = client.get_author_profile(name, affiliation)
+publications = client.get_publications(author_profile)
+from pprint import pprint
+
+pprint(publications)
